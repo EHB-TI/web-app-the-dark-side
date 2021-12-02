cf591fdc5465ab01a7b95a58277394017dc0a256
monkeyman-zero <44073137+monkeyman-zero@users.noreply.github.com>
2021-12-03 00:53:22 +0100

    redirect edit

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'PCController@index');
+//Route::get('/', 'PCController@index');
 Route::post('/predict', 'PCController@predict')->name("predict");
 Route::post('/suggestion', 'PCController@suggestion')->name("suggestion");
 
