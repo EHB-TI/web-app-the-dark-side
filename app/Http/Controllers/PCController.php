3c5a8654cbde7fe1b7a00f3d2e7f788dd9fbe0e3
MHennion <58027076+MHennion@users.noreply.github.com>
2021-12-03 15:15:46 +0100

    Update PCController.php

--- app/Http/Controllers/PCController.php
+++ app/Http/Controllers/PCController.php
@@ -23,7 +23,8 @@ class PCController extends Controller
 
     public function predict(Request $request)
     {
-        dd($get_token = GPU::where('token', $request->input('gpu'))->count());
+        $get_token = GPU::where('token', $request->input('gpu'))->count();
+        dd($get_token);
         if (GPU::where('GPU', '=', "\"" . $request->input('gpu') . "\"")->exists() > 0 && CPU::where('CPU', '=', $request->input('selectedCPU'))->exists() && Motherboard::where('Motherboard', '=', $request->input('motherboard'))->exists()) {
             $body['CPU'] = $request->input('selectedCPU');
             $body['Motherboard'] = $request->input('motherboard');
