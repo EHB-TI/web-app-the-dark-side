e6d4a0986284059a67baf128b6860e80f089d29e
MHennion <58027076+MHennion@users.noreply.github.com>
2021-12-03 15:41:25 +0100

    Input checks

--- app/Http/Controllers/PCController.php
+++ app/Http/Controllers/PCController.php
@@ -26,7 +26,6 @@ class PCController extends Controller
         $inputchecks = GPU::where('GPU', 'like', strtr($request->input('gpu'), array('(' => '%', ')' => '%')))->count();
         $inputchecks += CPU::where('CPU', 'like', strtr($request->input('selectedCPU'), array('(' => '%', ')' => '%')))->count();
         $inputchecks += Motherboard::where('Motherboard', 'like', strtr($request->input('motherboard'), array('(' => '%', ')' => '%')))->count();
-        dd($inputchecks);
 
         if ($inputchecks >= 3) {
             $body['CPU'] = $request->input('selectedCPU');
